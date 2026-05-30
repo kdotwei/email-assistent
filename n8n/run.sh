c703a984ac29c92510a2ce28ee8fae1a9461be00
kdotwei <kwei.mg09@nycu.edu.tw>
2026-05-30 16:46:09 +0800

    Add initial run script for n8n setup

---
+++ n8n/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+docker volume create n8n_data
+
+docker run -it --rm \
+    --name n8n \
+    -p 5678:5678 \
+    -e GENERIC_TIMEZONE="Asia/Taipei" \
+    -e TZ="Asia/Taipei" \
+    -e N8N_ENFORCE_SETTINGS_FILE_PERMISSIONS=true \
+    -e N8N_RUNNERS_ENABLED=true \
+    -v n8n_data:/home/node/.n8n \
+    docker.n8n.io/n8nio/n8n
